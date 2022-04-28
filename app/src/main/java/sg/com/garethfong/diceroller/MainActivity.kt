10dd3e5d1b1596ccb3538c96e27f867f0c326071
Gareth <garethfong@gmail.com>
2022-04-28 20:37:42 +0800

    initial commit

---
+++ app/src/main/java/sg/com/garethfong/diceroller/MainActivity.kt
@@ -0,0 +1,41 @@
+package sg.com.garethfong.diceroller
+
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener {
+//            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+            val diceView: ImageView = findViewById(R.id.imageView)
+            val dice = Dice(6).roll()
+            val d = when(dice){
+                1->R.drawable.dice_1
+                2->R.drawable.dice_2
+                3->R.drawable.dice_3
+                4->R.drawable.dice_4
+                5->R.drawable.dice_5
+                else->R.drawable.dice_6
+            }
+            diceView.setImageResource(d)
+            diceView.contentDescription = dice.toString()
+        }
+
+    }
+}
+
+class Dice(val sides: Int) {
+
+    fun roll(): Int {
+        return (1..sides).random()
+    }
+}
---
+++ app/src/test/java/sg/com/garethfong/diceroller/ExampleUnitTest.kt
@@ -0,0 +1,24 @@
+package sg.com.garethfong.diceroller
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun generates_numbers(){
+        val dice = Dice(6)
+        val result = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6",(result in 1..6))
+    }
+}
